1fe2c299a68235cca981fdde55936ed0a26913a8
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-11 16:52:57 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	 monopoly = {railroads: {pieces: 4, names: {one_piece_owned: 25, }, rent_in_dollars: {} } }
+	 monopoly = {railroads: {pieces: 4, names: {one_piece_owned: 25, two_pieces_owned: 50, }, rent_in_dollars: {} } }
 
 end
 
